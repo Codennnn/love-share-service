12b2ac00bc1001cb8d265b7c216dd3cc5c6e4970
令狐少侠 <czc12580520@gmail.com>
2019-11-13 17:06:55 +0800

    🚀 功能优化

--- app/service/user.js
+++ app/service/user.js
@@ -1,76 +1,77 @@
-'use strict';
+'use strict'
 
-const Service = require('egg').Service;
+const Service = require('egg').Service
 
 class UserService extends Service {
   async login(data) {
-    const { ctx, app } = this;
-    const { user_name, password } = data;
-    let res;
+    const { ctx, app } = this
+    const { account, password } = data
 
-    try {
-      res = await ctx.model.User.find({ user_name }).limit(1);
-    } catch (err) {
-      return { code: 3000, msg: err.message };
-    }
+    let res = await ctx.model.User.find({ phone: account }).limit(1)
 
-    if (res.length === 1) {
-      // 对比hash加密后的密码是否相等
-      const isMatch = await ctx.compare(password, res[0].password);
-
-      if (isMatch) {
-        // 创建JWT，有效期为7天
-        const token = app.jwt.sign(
-          { id: res[0]._id },
-          app.config.jwt.secret,
-          { expiresIn: '7d' }
-        );
-        return { code: 2000, msg: '登录校验成功', data: { token } };
+    if (res.length < 1) {
+      res = await ctx.model.User.find({ email: account }).limit(1)
+      if (res.length < 1) {
+        return { code: 4001, msg: '手机号或邮箱尚未注册' }
       }
-      return { code: 3000, msg: '登录校验失败' };
     }
-    return { code: 4004, msg: '该账号尚未注册' };
+    // 对比 hash 加密后的密码是否相等
+    const isMatch = await ctx.compare(password, res[0].password)
+
+    if (isMatch) {
+      // 创建JWT，有效期为7天
+      const token = app.jwt.sign(
+        { id: res[0]._id },
+        app.config.jwt.secret,
+        { expiresIn: '7d' }
+      )
+      return { code: 2000, msg: '登录校验成功', data: { token } }
+    }
+    return { code: 4003, msg: '密码错误' }
   }
 
   async createUser(data) {
-    const { ctx } = this;
-    const { user_name, password } = data;
-    const hash = await ctx.genHash(password);
-    let res;
+    const { ctx } = this
+    const { phone, password, nickname, real_name, school, roles = [ 'user' ] } = data
+    const hashPassword = await ctx.genHash(password)
+    let res
     await ctx.model.User
       .init()
       .then(async () => {
         res = await ctx.model.User
           .create({
-            user_name,
-            password: hash,
-            nick_name: data.nick_name,
-            roles: data.roles,
+            phone,
+            password: hashPassword,
+            nickname,
+            real_name,
+            school,
+            roles,
           })
           .then(() => {
-            return { code: 2001, msg: '创建用户成功' };
+            return { code: 2001, msg: '创建用户成功' }
           })
           .catch(err => {
+            console.log(err)
             if (err.message.includes('duplicate key error')) {
-              return { code: 3000, msg: '该账号已注册，请前往登录' };
+              return { code: 3000, msg: '该账号已注册，请前往登录' }
             }
-            return { code: 3000, msg: err.message };
-          });
-      });
-    return res;
+            return { code: 3000, msg: err.message }
+          })
+      })
+    return res
   }
 
   async deleteUser(id) {
-    const res = await this.ctx.model.User.deleteOne({ _id: id });
+    const res = await this.ctx.model.User.deleteOne({ _id: id })
     if (res.deletedCount === 1) {
-      return { code: 2004, msg: '删除用户成功' };
+      return { code: 2000, msg: '删除用户成功' }
     }
-    return { code: 3000, msg: '无删除任何用户' };
+    return { code: 3000, msg: '无删除任何用户' }
   }
 
   async getUsers() {
-    const res = await this.ctx.model.User.find();
-    return { code: 2000, msg: '查询所有用户', data: { users: res } };
+    const res = await this.ctx.model.User.find()
+    return { code: 2000, msg: '查询所有用户', data: { users: res } }
   }
 
   async updateUser(id, data) {
@@ -82,24 +83,24 @@ class UserService extends Service {
       )
       .then(res => {
         if (res.nModified === 1) {
-          return { code: 2004, msg: '数据更新成功' };
+          return { code: 2004, msg: '数据更新成功' }
         }
-        return { code: 3000, msg: '无可更新的数据' };
+        return { code: 3000, msg: '无可更新的数据' }
       })
       .catch(err => {
-        return { code: 3000, msg: err.message };
-      });
-    return res;
+        return { code: 3000, msg: err.message }
+      })
+    return res
   }
 
   async getUserInfo(id) {
     const res = await this.ctx.model.User.findOne(
       { _id: id },
       { nick_name: 1, roles: 1 }
-    );
+    )
 
-    return { code: 2000, msg: '获取用户登录信息', data: { ...res._doc } };
+    return { code: 2000, msg: '获取用户登录信息', data: { ...res._doc } }
   }
 }
 
-module.exports = UserService;
+module.exports = UserService
