5c1a61fa82c8af0249baff941ae05dbf53ad8b3b
令狐少侠 <czc12580520@gmail.com>
2019-11-13 17:06:01 +0800

    🔧 修改文件名

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: 'eslint-config-egg',
+  parser: 'babel-eslint',
+  rules: {
+    'generator-star-spacing': 'off',
+    'babel/generator-star-spacing': 'off',
+    semi: ["error", "never"]
+  }
+}
