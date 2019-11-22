c7358554bf3ef3e965d573de9665b2aec76300da
令狐少侠 <czc12580520@gmail.com>
2019-11-22 17:13:47 +0800

    🚀 功能优化

--- app/controller/goods.js
+++ app/controller/goods.js
@@ -2,6 +2,7 @@
 
 const Controller = require('egg').Controller
 
+
 class GoodsController extends Controller {
   /* GET
    * 获取商品列表
@@ -10,6 +11,31 @@ class GoodsController extends Controller {
     const { ctx, service } = this
     const data = ctx.request.body
     const res = await service.goods.getGoodsList(data)
+
+    ctx.body = res
+    ctx.status = 200
+  }
+
+  /* POST
+   * 图片上传
+   */
+  async uploadImg() {
+    const { ctx, service } = this
+    const id = ctx.state.user.id
+    const files = ctx.request.files
+    const res = await service.goods.uploadImg(id, files)
+
+    ctx.body = res
+    ctx.status = 200
+  }
+
+  /* DELETE
+   * 删除上传的图片
+   */
+  async deleteImg() {
+    const { ctx, service } = this
+    const { img_list } = ctx.request.body
+    const res = await service.goods.deleteImg(img_list)
 
     ctx.body = res
     ctx.status = 200
--- app/service/goods.js
+++ app/service/goods.js
@@ -1,6 +1,8 @@
 'use strict'
 
 const Service = require('egg').Service
+const fs = require('mz/fs')
+const path = require('path')
 
 class GoodsService extends Service {
   async getGoodsList(data) {
@@ -19,6 +21,42 @@ class GoodsService extends Service {
       total,
     }
     return { code: 2000, msg: '查询商品列表', data: { goods_list, pagination } }
+  }
+
+  async uploadImg(id, files) {
+    const { app } = this
+    const res = await Promise.all(files.map(async file => {
+      try {
+        return await app.fullQiniu.uploadFile(
+          `${id}-${path.basename(file.filename)}`,
+          file.filepath
+        )
+      } finally {
+        // 需要删除临时文件
+        await fs.unlink(file.filepath)
+      }
+    }))
+    if (res.every(el => el.ok)) {
+      return { code: 2000, msg: '所有图片上传成功' }
+    } else if (res.some(el => el.ok)) {
+      return { code: 5001, msg: '部分图片上传失败' }
+    }
+    return { code: 5000, msg: '图片上传失败' }
+  }
+
+  async deleteImg(imgList) {
+    const { app } = this
+    const res = await Promise.all(
+      imgList.map(
+        async imgName => await app.fullQiniu.delete(path.basename(imgName))
+      )
+    )
+    if (res.every(el => el.ok)) {
+      return { code: 2000, msg: '所有图片删除成功' }
+    } else if (res.some(el => el.ok)) {
+      return { code: 5001, msg: '部分图片删除失败' }
+    }
+    return { code: 5000, msg: '图片删除失败' }
   }
 }
 
